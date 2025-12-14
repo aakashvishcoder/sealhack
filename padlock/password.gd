2387affe433de9f0c2456e93954c0634bf389a2b
Aakash Vishnuvarth <121511679+aakashvishcoder@users.noreply.github.com>
2025-12-14 08:20:01 -0600

    padlock scene complete!

--- padlock/password.gd
+++ padlock/password.gd
@@ -1,5 +1,8 @@
 extends TextEdit
 
+@export var password: String = "abc"
+@export var level_finished_scene: String = ""
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	grab_focus()
@@ -12,4 +15,6 @@ func _process(delta: float) -> void:
 func _on_button_button_down() -> void:
 	var user_input = text
 	print("User entered: ", user_input)
+	if user_input == password:
+		get_tree().change_scene_to_file(level_finished_scene)
 	pass # Replace with function body.
