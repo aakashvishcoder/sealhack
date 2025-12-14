75ad0ed60a29633acf80ed200c77799ca56cb20a
Aakash Vishnuvarth <121511679+aakashvishcoder@users.noreply.github.com>
2025-12-13 21:47:32 -0600

    terminal complete!

--- terminal/clue_label.gd
+++ terminal/clue_label.gd
@@ -1,16 +1,10 @@
-extends Node2D
-
-@export var scene_path: String = ""
+extends RichTextLabel
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	text = Global.clue
 	pass # Replace with function body.
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
-
-func _on_button_button_down() -> void:
-	get_tree().change_scene_to_file(scene_path)
-	print("Moved to terminal scene: ", scene_path)
-	pass # Replace with function body.
