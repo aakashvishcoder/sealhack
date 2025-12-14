11500752a38cfd040bf46251a32cbc299b993846
Miguella297 <mikeivanov2010@gmail.com>
2025-12-13 23:31:37 -0500

    enemies done

--- menus/area_2d+1.gd
+++ menus/area_2d+1.gd
@@ -4,4 +4,14 @@ extends Area2D
 @export var target_scene_path : String = "res://deathscene.tscn"
 
 func _ready():
-	# Connect the signal (if not already 
+	# Connect the signal (if not already connected in the editor)
+	connect("body_entered", _on_body_entered)
+
+func _on_body_entered(body):
+	# Check if the body is in your player (assuming its a Kinematicbody2D or a Characterbody2D)
+	
+	if body.is_in_group("player"):
+		call_deferred("_fix")
+
+func _fix():
+	get_tree().change_scene_to_file(target_scene_path)
