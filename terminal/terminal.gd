f55ec81bf05549b3cc2e06018389a74ad94d4e0a
Aakash Vishnuvarth <121511679+aakashvishcoder@users.noreply.github.com>
2025-12-13 20:34:27 -0600

    fixed small issue with the question generation

--- terminal/terminal.gd
+++ terminal/terminal.gd
@@ -6,12 +6,15 @@ extends Node2D
 
 var question_list = {}
 var file_path = main_scene_file_path
+var random_index: int = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	question_list = load_json_file()
-	var random_index: int = randi_range(0, question_list["questions"].size())
+	random_index = randi_range(0, question_list["questions"].size()-1)
+	export_question()
 	var correct_answer: int = question_list["correct_answer"][random_index]
+	export_clue()
 	print(clue_scene_file_path)
 	match correct_answer:
 		1:
@@ -24,7 +27,7 @@ func _ready() -> void:
 			file_path = clue_scene_file_path
 			print("button 4")
 		4:
-			file_path = clue_scene_file_path
+			file_path = 'clue_scene_file_path'
 			print("button 5")
 		_:
 			print("No answer detected")
@@ -64,3 +67,13 @@ func _on_button_4_button_down() -> void:
 func _on_button_5_button_down() -> void:
 	print("button 5 pressed")
 	get_tree().change_scene_to_file(file_path)
+
+# exporting the clue to the clue scene
+func export_clue() -> void:
+	Global.clue = question_list["clues"][random_index]
+	print(Global.clue)
+	
+# exporting the question to the text label
+func export_question() -> void:
+	Global.question = question_list["questions"][random_index]
+	print(Global.question)
